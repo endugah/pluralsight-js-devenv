c45aee276fd30690b51720b8757227621c8a557d
endugah <endugah@spatialmapsconsult.net>
2019-07-20 18:43:38 +0300

    adjusted  Packages

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,29 +1,38 @@
 // jshint esversion:6
-import  express from './node_modules/express';
-import  path from 'path';
-import  open from './node_modules/open';
-import  webpack from './node_modules/webpack';
-
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
 
-const  port = 3000;
+const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
-app.use(require('./node_modules/webpack-dev-middleware')(compiler,{
-    noInfo:true,
-    publicPath: config.output.publicPath
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
 }));
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../src/index.html')); //declare routing 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err){ //tell express to listen to the port provided above
-    if(err){  
-        console.log(err);
-    } else {
-        open('http://localhost:' + port);
-    }
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
 });
